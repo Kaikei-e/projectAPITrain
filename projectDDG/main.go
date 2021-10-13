86dc5bd79840ab3e87f08f5411f6203403ae5744
Kaikei-e <e_kaikei@protonmail.com>
2021-10-13 13:50:28 +0000

    Init: dir

--- projectDDG/main.go
+++ projectDDG/main.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main(){
 	router :=	gin.Default()
 
 	router.LoadHTMLGlob("templates/")
+
+	
+	router.NoRoute(func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently ,"/")
+	})
+	
+	router.GET("/", func (c *gin.Context)  {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+
+		})
+	})
+  
+	router.Run(":8085") // listen and serve on 0.0.0.0:8085 (for windows "localhost:8085")
 }
